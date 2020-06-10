6d2e81754efaf1af9bd0446995e1e51afb6200a7
Diego Casas <dacasas@uninorte.edu.co>
2020-06-10 14:05:59 -0500

    Code refactoring

--- GMDApp.jl
+++ GMDApp.jl
@@ -1,7 +1,8 @@
 # GMDApp.jl (WIP)
 
-import Base.parse, Base.getindex
-import DelimitedFiles
+import Base: parse, getindex
+using DelimitedFiles: readdlm
+
 
 struct Record
     RSN::Int64
@@ -64,70 +65,117 @@ function getindex(rec::Record, k::AbstractString)
     getproperty(rec, Symbol(k))
 end
 
-function parse(::Type{Tuple{T}}, s::AbstractString) where T
+
+function parse(::Type{Tuple{T}}, s::AbstractString) where {T}
     Tuple(parse.(T, split(s, ',')))
 end
 
 
-io = open("test/NGA-Sub_Intraslab.csv")
-nrec = countlines(io) - 4
-seekstart(io)
-readline(io)
-n = parse(Int64, readline(io))
-T = parse.(Float64, split(readline(io), ','))
-readline(io)
-records = Array{Record,1}(undef, nrec)
-for (i, line) in enumerate(eachline(io))
-    row = split(line, ',')
-    records[i] = Record(
-        RSN = parse(Int64, row[1]),
-        EQID = parse(Int64, row[2]),
-        SSN = parse(Int64, row[3]),
-        FaultType = 999,
-        Magnitude = parse(Float64, row[4]),
-        Rjb = parse(Float64, row[8]),
-        ClstD = parse(Float64, row[7]),
-        Vs30 = parse(Float64, row[13]),
-        PGA = parse(Float64, row[20]),
-        PGV = parse(Float64, row[21]),
-        PGD = parse(Float64, row[22]),
-        Dur = parse(Float64, row[17]),
-        S = parse.(Float64, row[23:133]),
-        FileNameHorizontal1 = row[134],
-        FileNameHorizontal2 = row[135],
-        FileNameVertical = row[136],
-    )
-end
-close(io)
-
-
-io = open("test/filter")
-filterdict = Dict()
-for line in eachline(io)
-    k, v = split(line)
-    if v != "NA"
-        if k == "FaultType"
-            push!(filterdict, k => parse(Int64, v))
-        else
-            push!(filterdict, k => parse(Tuple{Float64}, v))
+function read_records(filename)
+    open(filename) do io
+        nrec = countlines(io) - 3
+        seekstart(io)
+        readline(io) # title
+        T = parse.(Float64, split(readline(io), ','))
+        readline(io) # header
+        records = Array{Record,1}(undef, nrec)
+        for (i, line) in enumerate(eachline(io))
+            row = split(line, ',')
+            records[i] = Record(
+                RSN = parse(Int64, row[1]),
+                EQID = parse(Int64, row[2]),
+                SSN = parse(Int64, row[3]),
+                FaultType = 999,
+                Magnitude = parse(Float64, row[4]),
+                Rjb = parse(Float64, row[8]),
+                ClstD = parse(Float64, row[7]),
+                Vs30 = parse(Float64, row[13]),
+                PGA = parse(Float64, row[20]),
+                PGV = parse(Float64, row[21]),
+                PGD = parse(Float64, row[22]),
+                Dur = parse(Float64, row[17]),
+                S = parse.(Float64, row[23:133]),
+                FileNameHorizontal1 = row[134],
+                FileNameHorizontal2 = row[135],
+                FileNameVertical = row[136],
+            )
         end
-    end
-end
-close(io)
-
-is = ones(Bool, nrec)
-for (i, rec) in enumerate(records)
-    for (k, v) in filterdict
-        if k == "FaultType"
-            rec[k] != v && (is[i] &= false)
-        else
-            !(first(v) ≤ rec[k] ≤ last(v)) && (is[i] &= false)
-        end
+        records, T
     end
 end
 
 
-obj = DelimitedFiles.readdlm("test/example_spectra.csv", ',', Float64, skipstart=2)
-Tobj = obj[:, 1]
-Sobj = obj[:, 2]
+function read_filters(filename)
+    filters = Dict()
+    open("test/filter") do io
+        for line in eachline(io)
+            k, v = split(line)
+            if v != "NA"
+                if k == "FaultType"
+                    push!(filters, k => parse(Int64, v))
+                else
+                    push!(filters, k => parse(Tuple{Float64}, v))
+                end
+            end
+        end
+    end
+    function f(rec)
+        for (k, v) in filters
+            # returns false at the firt false condition
+            # i.e. ALL conditions must be satified
+            if k == "FaultType"
+                rec[k] != v && return false
+            else
+                !(first(v) ≤ rec[k] ≤ last(v)) && return false
+            end
+        end
+        true
+    end
+end
 
+
+function read_target_spectrum(filename)
+    data = readdlm(filename, ',', Float64, skipstart = 2)
+    T = data[:, 1]
+    S = data[:, 2]
+    T, S
+end
+
+
+function loginterp(x, xp, yp)
+    n = length(x)
+    m = length(xp)
+    y = Vector{Float64}(undef, n)
+    j = 1
+    for i in 1:n
+        if x[i] < xp[begin]
+            y[i] = yp[begin]
+        elseif x[i] > xp[end]
+            y[i] = yp[end]
+        else
+            while j < m
+                if xp[j] ≤ x[i] ≤ xp[j+1]
+                    k = log(yp[j+1] / yp[j]) / log(xp[j+1] / xp[j])
+                    y[i] = exp(k * log(x[i] / xp[j]) + log(yp[j]))
+                    # The next point could be in the same bracket,
+                    # so we break the loop without accumulating j.
+                    break
+                end
+                j += 1
+            end
+        end
+    end
+    y
+end
+
+# function main()
+
+records, T = read_records("test/NGA-Sub_Intraslab.csv")
+
+criteria = read_filters("test/filter")
+filter!(criteria, records)
+
+T_target, S_target = read_target_spectrum("test/example_spectra.csv")
+S_target = loginterp(T, T_target, S_target)
+
+# end
