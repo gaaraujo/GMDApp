a90ac5a19e7db2a4368525880f8c66ba0fd8f316
Diego Casas <dacasas@uninorte.edu.co>
2020-06-11 02:16:57 -0500

    Minor fixes and refactoring. Add test plots.

--- GMDApp.jl
+++ GMDApp.jl
@@ -3,6 +3,9 @@
 import Base: parse, getindex
 using DelimitedFiles: readdlm
 
+using Plots
+using Statistics
+
 
 struct Record
     RSN::Int64
@@ -77,8 +80,8 @@ function read_records(filename)
         seekstart(io)
         readline(io) # title
         T = parse.(Float64, split(readline(io), ','))
-        readline(io) # header
-        records = Array{Record,1}(undef, nrec)
+        readline(io) # TODO: header
+        records = Vector{Record}(undef, nrec)
         for (i, line) in enumerate(eachline(io))
             row = split(line, ',')
             records[i] = Record(
@@ -107,11 +110,11 @@ end
 
 function read_filters(filename)
     filters = Dict()
-    open("test/filter") do io
+    open(filename) do io
         for line in eachline(io)
             k, v = split(line)
             if v != "NA"
-                if k == "FaultType"
+                if k == "FaultType" # TODO: FaultType can be a collection
                     push!(filters, k => parse(Int64, v))
                 else
                     push!(filters, k => parse(Tuple{Float64}, v))
@@ -123,13 +126,14 @@ function read_filters(filename)
         for (k, v) in filters
             # returns false at the firt false condition
             # i.e. ALL conditions must be satified
-            if k == "FaultType"
+            if k == "FaultType" # TODO: Check for membership
                 rec[k] != v && return false
             else
                 !(first(v) ≤ rec[k] ≤ last(v)) && return false
             end
         end
-        true
+        # check that all the spectrum is positive
+        !any(rec.S .≤ 0)
     end
 end
 
@@ -147,11 +151,15 @@ function loginterp(x, xp, yp)
     m = length(xp)
     y = Vector{Float64}(undef, n)
     j = 1
+    first_xp = first(xp)
+    first_yp = first(yp)
+    last_xp = last(xp)
+    last_yp = last(yp)
     for i in 1:n
-        if x[i] < xp[begin]
-            y[i] = yp[begin]
-        elseif x[i] > xp[end]
-            y[i] = yp[end]
+        if x[i] < first_xp
+            y[i] = first_yp
+        elseif x[i] > last_xp
+            y[i] = last_yp
         else
             while j < m
                 if xp[j] ≤ x[i] ≤ xp[j+1]
@@ -168,41 +176,69 @@ function loginterp(x, xp, yp)
     y
 end
 
-# function main()
+function main()
+    records, T = read_records("test/NGA-Sub_Intraslab_FullSet.csv")
 
-records, T = read_records("test/NGA-Sub_Intraslab.csv")
+    n = length(T)
 
-n = length(T)
+    criteria = read_filters("test/filter")
+    filter!(criteria, records)
 
-criteria = read_filters("test/filter")
-filter!(criteria, records)
+    nrec = length(records)
 
-nrec = length(records)
+    T_target, S_target = read_target_spectrum("test/spectra.csv")
+    S_target = loginterp(T, T_target, S_target)
 
-T_target, S_target = read_target_spectrum("test/example_spectra.csv")
-S_target = loginterp(T, T_target, S_target)
+    # TODO: implement weights in the spectrum file
+    # T_w = [0.01, 1, 10, 20]
+    # w = [1, 1, 1, 1]
+    # T_w = [0.01, 0.09, 0.1, 0.11, 0.99,  1, 1.01,  10,    20   ]
+    # w =   [0.001, 0.001, 1, 0.9, 0.9, 1, 0.001, 0.001, 0.001]
+    T_w = [0.01, 0.09, 0.1, 0.11, 0.99,  1, 1.01,  10,    20   ]
+    w =   [0.001, 0.001, 1, 0.001, 0.001, 1, 0.001, 0.001, 0.001]
+    w = loginterp(T, T_w, w)
+    w[(T .< first(T_target)) .& (T .> last(T_target))] .= 0
+    w ./= sum(w)
 
-T_w = [0.01, 1, 10, 20]
-w = [1, 1, 1, 1]
-w = loginterp(T, T_w, w)
-w ./= sum(w)
+    f = Vector{Float64}(undef, nrec)
+    e² = Vector{Float64}(undef, nrec)
+    for (i, rec) in enumerate(records)
+        f[i] = exp(sum(w .* log.(S_target ./ rec.S)))
+        e²[i] = sum(w .* log.(S_target ./ (f[i] .* rec.S)).^2)
+    end
 
-f = Vector{Float64}(undef, nrec)
-e² = Vector{Float64}(undef, nrec)
-for (i, rec) in enumerate(records)
-    f[i] = exp(sum(w .* log.(S_target ./ rec.S)))
-    e²[i] = sum(w .* log.(S_target ./ (f[i] .* rec.S)).^2)
+    # using Plots
+
+    k = 11 # TODO: read this number from the filter file
+    # TODO: read Scale Factor range from the filter file
+    # TODO: check for empty array after filtering
+    ibest = partialsortperm(e², 1:k)
+    plt = plot(
+        T,
+        S_target,
+        xaxis = :log,
+        yaxis = :log,
+        legend = false,
+        xlim = (T_target[1], T_target[end])
+    )
+    S_best = Matrix{Float64}(undef, n, length(ibest))
+    for (j, i) in enumerate(ibest)
+        rec = records[i]
+        S_best[:, j] .= f[i] .* rec.S
+        println("RSN = $(rec.RSN), e² = $(e²[i])")
+        plot!(plt, T, S_best[:, j], c = "gray")
+    end
+    S_mean = Vector{Float64}(undef, n)
+    S_std = Vector{Float64}(undef, n)
+    for i = 1:n
+        S_mean[i] = mean(S_best[i, :])
+        S_std[i] = exp(std(log.(S_best[i, :])))
+    end
+    plot!(plt, T, S_mean, c = "black", lw = 3)
+    plot!(plt, T, S_mean .* S_std, c = "black", lw = 3, s = :dot)
+    plot!(plt, T, S_mean ./ S_std, c = "black", lw = 3, s = :dot)
+    plot!(plt, T, S_target, c = "red", lw = 3)
+    plt
 end
 
-using Plots
-
-k = 20
-ibest = partialsortperm(e², 1:k)
-plt = plot(T, S_target, xaxis = :log, yaxis = :log, legend = false)
-for i in ibest
-    rec = records[i]
-    println("RSN = $(rec.RSN), e² = $(e²[i])")
-    plot!(plt, T, f[i] * rec.S, color = "gray")
-end
-plot!(plt, T, S_target, color = "red", linewidth = 5)
-# end
+@time plt = main()
